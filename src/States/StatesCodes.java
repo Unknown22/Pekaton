b996e1f96f55feb72c1a51d300b7e4e9f41b4aa0
Dominik Nowak <nowik2@gmail.com>
2016-06-04 19:27:58 +0200

    add new state - LoginState and method to getIdUser by login and password

--- src/States/StatesCodes.java
+++ src/States/StatesCodes.java
@@ -5,5 +5,6 @@ package States;
 		//public static int MENU = 0;
 		public static int GAMESTATE = 0;
 		public static int WORKERSTATE = 1;
+		public static int LOGINSTATE = 2;
 
 }
--- src/core/ClassesInstances.java
+++ src/core/ClassesInstances.java
@@ -1,12 +1,14 @@
 package core;
 
 import States.GameState;
+import States.LoginState;
 import States.WorkerStatState;
 
 public class ClassesInstances {
 
 	public static GameState gameState;
 	public static WorkerStatState workerStatState;
+	public static LoginState loginState;
 
 	
 }
