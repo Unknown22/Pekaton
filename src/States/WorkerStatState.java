8f469ec8df9aad458b0271936f935000ca0b9c05
Dominik Nowak <nowik2@gmail.com>
2016-06-04 19:08:51 +0200

    add monitor view in tasks manager

--- src/States/WorkerStatState.java
+++ src/States/WorkerStatState.java
@@ -10,11 +10,15 @@ import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.TrueTypeFont;
+import org.newdawn.slick.UnicodeFont;
+import org.newdawn.slick.font.effects.ColorEffect;
 import org.newdawn.slick.geom.Rectangle;
 import org.newdawn.slick.geom.Shape;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 
+import GameElements.Worker;
+import core.Resources;
 import core.Window;
 import model.Pracownik;
 import model.Zadanie;
@@ -23,34 +27,38 @@ import database.DataBase;
 public class WorkerStatState extends BasicGameState {
 
 	DataBase db = new DataBase();
-	int margin=30;
+	int margin=40;
 	int cellMargin=10;
 	Pracownik pracownik=db.getPracownikByID(1);
 	ArrayList<Zadanie> zadania = (ArrayList<Zadanie>) db.getZadaniaByPracownikId(1);
-	Shape tableAll=new Rectangle(margin, margin, Window.width-margin*2, Window.height-margin*2);
 	int cellWidth=(Window.width-margin*2)/3;
 	int cellWidth2=(Window.width-margin*2)/4;
-	Font font = new Font("Poster Bodoni", Font.PLAIN, 20);
-	TrueTypeFont ttf = new TrueTypeFont(font, true);
+	UnicodeFont font = new UnicodeFont(new Font("Minecraftia", Font.PLAIN, 20));
+	int i=0;
 	
 	
 	
 	@Override
 	public void init(GameContainer gc, StateBasedGame sbg) throws SlickException {
 		// TODO Auto-generated method stub
-		
+		font.addGlyphs("¹æ³óê¿Ÿñœ");
+		font.getEffects().add(new ColorEffect(java.awt.Color.white));
+		font.addGlyphs("¹æ³óê¿Ÿñœ"); // szczególnie wa¿na jest ta linijka bo
+											// to ona dodaje polskie znaki
+		font.addNeheGlyphs();
+		font.loadGlyphs();
 	}
 
 	@Override
 	public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException {
 		// TODO Auto-generated method stub
-		g.setColor(Color.blue);
-		g.draw(tableAll);
 		
-		g.setFont(ttf);
+		g.drawImage(Resources.getSpritesheet("monitor").getSubImage(0, 0, Window.width, Window.height), 0 ,0);
+		
+		g.setFont(font);
 		g.setColor(Color.blue);
 		g.drawString(pracownik.getLogin(), Window.width/2-margin, 50);
-		g.setColor(Color.cyan);
+		g.setColor(Color.black);
 		g.drawString("ID", margin+cellMargin, 100);
 		g.drawString("Stanowisko", margin+cellMargin+cellWidth, 100);
 		g.drawString("Doœwiadczenie", margin+cellMargin+cellWidth*2, 100);
@@ -64,26 +72,24 @@ public class WorkerStatState extends BasicGameState {
 		g.drawString("Zadania", Window.width/2-margin, 200);
 		
 		
-		for(int i=0; i<zadania.size(); i++){
-			g.setColor(Color.cyan);
-			g.drawString(zadania.get(i).getOpis(), margin+cellMargin, 250+i*100);
-			g.setColor(Color.darkGray);
-			g.drawString(Integer.toString(zadania.get(i).getDoswiadczenie()), margin+cellMargin, 275+i*100);
-			g.drawString(zadania.get(i).getZleceniodawca(), margin+cellMargin, 300+i*100);
-			if(zadania.get(i).getStatus()==0)
-			{
-				g.setColor(Color.red);
-				g.drawString("Niewykonane", margin+cellMargin, 325+i*100);
-			}
-				
-			else
-			{
-				g.setColor(Color.green);
-				g.drawString("Wykonane", margin+cellMargin, 325+i*100);
-			}
-				
-			g.drawLine(margin, 350+i*100, Window.width-margin, 350+i*100);
+		g.setColor(new Color(0x1E1B68));
+		g.drawString(zadania.get(i).getOpis(), margin+cellMargin, 250);
+		g.setColor(Color.darkGray);
+		g.drawString(Integer.toString(zadania.get(i).getDoswiadczenie()), margin+cellMargin, 275);
+		g.drawString(zadania.get(i).getZleceniodawca(), margin+cellMargin, 300);
+		if(zadania.get(i).getStatus()==0)
+		{
+			g.setColor(Color.red);
+			g.drawString("Niewykonane", margin+cellMargin, 325);
 		}
+			
+		else
+		{
+			g.setColor(new Color(0x12902B));
+			g.drawString("Wykonane", margin+cellMargin, 325);
+		}
+				
+
 		
 		
 	}
@@ -93,6 +99,18 @@ public class WorkerStatState extends BasicGameState {
 		// TODO Auto-generated method stub
 		if(gc.getInput().isKeyPressed(Input.KEY_L) || gc.getInput().isKeyPressed(Input.KEY_ESCAPE)){
 			sbg.enterState(StatesCodes.GAMESTATE);
+		}
+		if (gc.getInput().isKeyPressed(Input.KEY_W) || gc.getInput().isKeyPressed(Input.KEY_UP))
+		{
+			if(i+1<zadania.size())
+				i++;
+		}
+		
+		if (gc.getInput().isKeyPressed(Input.KEY_S) || gc.getInput().isKeyPressed(Input.KEY_DOWN))
+		{
+			if(i>0)
+				i--;
+				
 		}
 	}
 
